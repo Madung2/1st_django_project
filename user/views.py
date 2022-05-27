4bd0a0928cf9cdaf4df2b857ce0b86339487aac9
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-05-27 10:37:05 +0900

    로그인 기능 리팩토링, tweet페이지추가

--- user/views.py
+++ user/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import UserModel
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+#사용자가 데이터베이스 안에 있는지 검사하는 함수
+from django.contrib import auth
 
 # Create your views here.
 def sign_up_view(request):
@@ -16,25 +19,24 @@ def sign_up_view(request):
         #2. 패스워드 2개가 다른지 같은지
 
         if password != password2:
-            return render(request, 'user/signin.html')
+            return render(request, 'user/signup.html')
         else:
-            new_user = UserModel()
-            new_user.username = username
-            new_user.password = password
-            new_user.bio = bio
-            new_user.save()
-        return redirect('/sign-in')
+            user_exist = get_user_model().objects.filter(username=username)
+            if user_exist:
+                return render(request, 'user/signup.html')
+            else:
+                UserModel.objects.create_user(username=username, password=password, bio=bio)
+                return redirect('/sign-in')
 
 
 def sign_in_view(request):
     if request.method == 'POST':
         username = request.POST.get('username', None) #html의 input->name과 동일하게 작성
         password = request.POST.get('password', None)
-
-        me = UserModel.objects.get(username=username)
-        if me.password == password:
-            request.session['user'] = me.username
-            return HttpResponse("로그인 성공!")
+        me = auth.authenticate(request, username=username, password=password)
+        if me is not None:
+            auth.login(request, me)
+            return redirect('/')
         else:
             return redirect('/sign-in')
     elif request.method == 'GET':
