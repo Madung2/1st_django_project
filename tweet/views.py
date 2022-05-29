9e93bba0451b8e24e439ad7371e1f114e08bb264
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-05-29 17:37:29 +0900

    게시물 생성/삭제기능 -3주차 완/3주차 숙제 시작전

--- tweet/views.py
+++ tweet/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from .models import TweetModel
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -15,6 +17,22 @@ def tweet(request):
     if request.method == 'GET':
         ur_user = request.user.is_authenticated
         if ur_user:
-            return render(request, 'tweet/home.html')
+            all_tweet = TweetModel.objects.all().order_by('-created_at')
+            return render(request, 'tweet/home.html', {'tweet': all_tweet}) #요 트윗 키값은 html 템플릿에서 사용됨
         else:
             return redirect('/sign-in')
+
+    elif request.method =='POST':
+        user = request.user #로그인된 사용자의 전체 정보
+        my_tweet = TweetModel() #이거 왠지 필수...나제?!?! : 클래스는 빵틀이라서
+        my_tweet.author = user
+        my_tweet.content = request.POST.get('my-content', '') #my-content는 html form name
+        my_tweet.save()
+        return redirect('/tweet')
+
+@login_required()
+def delete_tweet(request, id):
+    my_tweet = TweetModel.objects.get(id=id) #url에 id를 넘겨줘야함
+    my_tweet.delete()
+    return redirect('/tweet')
+
