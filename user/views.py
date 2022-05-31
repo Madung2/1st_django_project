3d84e58935807094d6f1c5ec490aae5ffcb44d88
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-05-31 13:55:21 +0900

    2주차 완성

--- user/views.py
+++ user/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import UserModel
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 def sign_up_view(request):
-    return render(request, 'user/signup.html')
+    if request.method=='POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        password2 = request.POST.get('password2', None)
+        bio = request.POST.get('bio', None)
+
+        exist_user = get_user_model().objects.filter(username=username)
+        if password != password:
+            return render(request, 'user/signup.html')
+        elif exist_user:
+            return render(request, 'user/signup.html')
+        else:
+            new_user = UserModel()
+            new_user.username = username
+            new_user.password = password
+            new_user.bio = bio
+            new_user.save()
+        return redirect('/sign-in')
+
+    elif request.method == 'GET':
+        return render(request, 'user/signup.html')
 
 
 def sign_in_view(request):
-    return render(request, 'user/signin.html')
+    if request.method == 'POST':
+        return HttpResponse("로그인 성공")
+    elif request.method == 'GET':
+
+        return render(request, 'user/signin.html')
